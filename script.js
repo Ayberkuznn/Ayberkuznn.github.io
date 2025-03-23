bd087d8ea8f221e9ca99c0f0fab3ff48b4321311
Ayberkuznn <163853423+Ayberkuznn@users.noreply.github.com>
2025-03-23 12:01:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("convert-btn").addEventListener("click", function() {
+    const amount = document.getElementById("amount").value;
+    const fromCurrency = document.getElementById("from-currency").value;
+    const toCurrency = document.getElementById("to-currency").value;
+
+    if(amount === "") {
+        alert("Lütfen bir miktar girin.");
+        return;
+    }
+
+    fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`)
+    .then(response => response.json())
+    .then(data => {
+        const convertedAmount = data.rates[toCurrency];
+        document.getElementById("result").innerHTML = 
+        `${amount} ${fromCurrency} = ${convertedAmount.toFixed(3)} ${toCurrency}`;
+    })
+    .catch(error => {
+        console.error("Hata:", error);
+        document.getElementById("result").innerHTML = "Bir hata oluştu.";
+    });
+});
